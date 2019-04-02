764cff2d96cf158062e1deb3b77de5e962bc22a0
zhangyk <zhangyk@tju.edu.cn>
2019-04-02 14:34:04 +0800

    This is Event/delegate example used in WPF. It also demo the main/sub window exchange data by this keyword. Modal/None Modal window

---
+++ WpfApp3/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //this.OK.Click += new System.Windows.RoutedEventHandler(OK_Click);
+
+        }
+
+        private void OK_Click(object sender, RoutedEventArgs e)
+        {
+            //SubWindow subwin = new SubWindow(sender);
+            //subwin.Show();
+            SubWindow subwin = new SubWindow(this);
+            subwin.ShowDialog();
+
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            MessageBox.Show("I am closed!");
+            
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            //MessageBox.Show("I am closing!");
+            //e.Cancel = true;//prevent window closing
+            string msg = "Data is dirty. Close without saving?";
+            MessageBoxResult result =
+              MessageBox.Show(
+                msg,
+                "Window closing: Data App",
+                MessageBoxButton.YesNo,
+                MessageBoxImage.Warning);
+            if (result == MessageBoxResult.No)
+            {
+                // If user doesn't want to close, cancel closure
+                e.Cancel = true;
+            }
+        }
+    }
+}
---
+++ WpfApp3/SubWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// SubWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class SubWindow : Window
+    {
+        public SubWindow()
+        {
+            InitializeComponent();
+        }
+        public SubWindow(object obj)
+        {
+            InitializeComponent();
+            var btn=  obj as Button;
+            if(btn!=null)
+            {
+                TxtBlkHello.Text = btn.Name.ToString()+"== "+btn.Content.ToString();
+            }
+            else
+            {
+                //not a button
+                var mwin = obj as MainWindow;
+                if(mwin!=null)
+                {
+                    mwin.Title = $"SetBy:{this.Title.ToString()+" =="+this.GetType().ToString()}: I am a MainWindow!";
+                    this.Title = "SubWindow is a Modal window, you can not switch to MainWindow when I am aliving";
+                }
+
+            }
+        }
+
+        private void BtnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
